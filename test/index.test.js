04a0b40aa9dbc56d2ae1b02498b2d39b044f613a
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-15 19:59:42 +0100

    Added another simple test

--- test/index.test.js
+++ test/index.test.js
@@ -5,4 +5,8 @@ describe('test', () => {
   it('should return a string', () => {
     expect('ci with travis a simple change').to.equal('ci with travis a simple change');
   });
+
+  it('should return a string', () => {
+    expect('This is another test').to.equal('This is another test');
+  });
 });
