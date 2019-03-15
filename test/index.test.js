6aa6affccf55034abcb501a2cf8ac82bb16f4d02
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-15 20:02:15 +0100

    Another another testcase

--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,15 @@ const expect = require('chai').expect;
 const server = require('./index.test');
 
 describe('test', () => {
-  it('should return a string', () => {
+  it('first test', () => {
     expect('ci with travis a simple change').to.equal('ci with travis a simple change');
   });
 
-  it('should return a string', () => {
+  it('second test', () => {
     expect('This is another test').to.equal('This is another test');
+  });
+
+  it('third test', () => {
+    expect('Testing if it will build from a merge').to.equal('Testing if it will build from a merge');
   });
 });
